0b83f746a1ca54b8be12a5fd86dfaa9deeaf8d48
Gabriel Bittencourt Camillo de Souzae <Gabrielbcds40@gmail.com>
2024-08-25 14:46:04 -0300

    carrinho quase completo

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ function navigateTo(href) {
   window.location.hash = href;
 }
 
+//----------------------------------------------------------------------------------
+//consumo da api e criação de produtos no HTML
 document.addEventListener('DOMContentLoaded', function () {
   let productSection = document.querySelector('.products-section');
+
 
   async function fetchProdutos(url) {
     let data = await fetch(url);
     let response = await data.json();
+
     console.log(response);
 
     response.forEach(product => {
       const rating = product.rating.rate;
 
-      
-      
       const productHtml = `
         <div class="product">
           <div class="imagem-container">
@@ -23,36 +25,23 @@ document.addEventListener('DOMContentLoaded', function () {
           </div>
           <div class="info-product">
             <h1 class="titulo">${product.title}</h1>
-
-
-            
-
-
-
             <div class="stars" data-rating="${rating}">
-
               <span class="star" data-value="1"><i class="fa-solid fa-star"></i></span>
               <span class="star" data-value="2"><i class="fa-solid fa-star"></i></span>
               <span class="star" data-value="3"><i class="fa-solid fa-star"></i></span>
               <span class="star" data-value="4"><i class="fa-solid fa-star"></i></span>
               <span class="star" data-value="5"><i class="fa-solid fa-star"></i></span>
-
-
-                 <span class="nota">${product.rating.rate}</span>
+              <span class="nota">${product.rating.rate}</span>
             </div>
-
             <p>${product.rating.count} Avaliações</p>
             <p class="preco">R$${product.price}</p>
-            <button><img src="img/icons8-carrinho-48 (3).png"></button>
+            <button onclick="addCarrinho(${product.id})"><img src="img/icons8-carrinho-48 (3).png"></button>
           </div>
         </div>
       `;
 
-    
-      
       productSection.innerHTML += productHtml;
 
-     
       const stars = productSection.querySelector(`.product:last-child .stars`);
       const starElements = stars.querySelectorAll('.star');
 
@@ -71,21 +60,29 @@ document.addEventListener('DOMContentLoaded', function () {
   fetchProdutos('https://fakestoreapi.com/products');
 });
 
+
+
+
+//----------------------------------------------------------------------------------
+//Abrir perfil e carrinho
 function acao() {
   event.preventDefault()
   let janela = document.querySelector('.janela-carrinho');
   let sombra = document.querySelector('.sombra');
 
 
+   janela.classList.toggle('active')
+
  
-  if (janela.style.display === 'none' || janela.style.display === '') {
-    janela.style.display = 'block'; 
+
+  if (sombra.style.display === 'none' || sombra.style.display === '') {
+ 
     sombra.style.display = 'block'; 
  
 
   } else {
     sombra.style.display = 'none';
-    janela.style.display = 'none'; 
+  
   }
      
 }
@@ -94,6 +91,7 @@ function abrirPerfil(){
   event.preventDefault()
   let login_register = document.querySelector('.login-register')
   let sombra = document.querySelector('.sombra');
+
   login_register.classList.toggle('active')
 
   if (sombra.style.display === 'none' || sombra.style.display === '') {
@@ -104,6 +102,9 @@ function abrirPerfil(){
   }
 }
 
+
+//----------------------------------------------------------------------------------
+//banner slider
 let count = 1;
 
 document.getElementById('item1').checked = true
@@ -119,4 +120,59 @@ function nextImage(){
   }
 
   document.getElementById('item'+ count).checked = true
+}
+//----------------------------------------------------------------------------------
+//adicionar ao carrinho
+let itens = []
+
+let itensCheck = []
+
+let no_item = document.querySelector('.no-item')
+let containerItem = document.querySelector('.itens-carrinho')
+let carrinhoButton = document.querySelector('.fechar-pedido')
+
+function teste(){
+  carrinhoButton.classList.toggle('active', itens.length > 0);
+  containerItem.classList.toggle('active', itens.length > 0);
+
+  no_item.classList.add('active', itens.length === 0);
+}
+
+
+function addCarrinho(produtoId) {
+  event.preventDefault();
+
+  let itens_section = document.querySelector('.itens-carrinho')
+  async function fetchProduto(url) {
+
+
+      let data = await fetch(url);
+    
+      let produto = await data.json();
+      itens.push(produto);  
+      itensCheck.push(produto)
+      teste()
+      console.log(itensCheck)
+      console.log(itens);
+
+      itens.forEach(produto =>{
+        const itensHTML = `
+            <div class="item-carrinho">
+
+           <img src="${produto.image}" alt="">
+           <input type="checkbox">
+            <h2 class="titulo">${produto.title}</h2>
+             <p class="price">Preço: ${produto.price}</p>
+              <p class="quantidade">Quantidade: 10</p>
+                <span><i class="fa-solid fa-x"></i></span>
+          
+         </div>
+        `
+        itens = []
+        itens_section.innerHTML += itensHTML;
+      })
+   
+  }
+
+  fetchProduto(`https://fakestoreapi.com/products/${produtoId}`);
 }
